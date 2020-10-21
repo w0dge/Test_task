07c256221fbb8089a0dfb4b0852e9509bf67da80
w0dge <wodgeonixias@mail.ru>
2020-10-21 22:20:08 +0300

    Initial commit+added uses-permisson, also added implementations of retrofit+gson

---
+++ app/src/main/java/wodge/programming/testtaskfromarcanit/MainActivity.kt
@@ -0,0 +1,11 @@
+package wodge.programming.testtaskfromarcanit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
