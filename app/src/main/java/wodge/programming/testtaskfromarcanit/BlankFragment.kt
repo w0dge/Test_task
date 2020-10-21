07c4ae66aab44e0b2f8e79beeb384bf3ad1269fb
w0dge <wodgeonixias@mail.ru>
2020-10-21 23:04:41 +0300

    Created fragment and view of content in it.

---
+++ app/src/main/java/wodge/programming/testtaskfromarcanit/BlankFragment.kt
@@ -0,0 +1,31 @@
+package wodge.programming.testtaskfromarcanit
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BlankFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BlankFragment()
+    }
+
+    private lateinit var viewModel: BlankViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_info, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(BlankViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/wodge/programming/testtaskfromarcanit/BlankViewModel.kt
@@ -0,0 +1,7 @@
+package wodge.programming.testtaskfromarcanit
+
+import androidx.lifecycle.ViewModel
+
+class BlankViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
